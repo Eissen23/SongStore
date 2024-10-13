0e3a8571780e648cc33a71d04e1084ec6be51f34
Phạm Đức Phúc <87574901+Eissen23@users.noreply.github.com>
2024-10-13 21:54:46 +0700

    ok good to go

---
+++ WebApplication1/Models/Playlist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models;
+
+public partial class Playlist
+{
+    public int PlaylistId { get; set; }
+
+    public string? Name { get; set; }
+
+    public virtual ICollection<Track> Tracks { get; set; } = new List<Track>();
+}
